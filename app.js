7f22aafef1b4901f752132908bd69d0def7e47e4
micro-hawk <dasvikas0071@gmail.com>
2021-08-01 22:28:13 +0530

    Data Binding VUE 2

---
+++ app.js
@@ -0,0 +1,12 @@
+new Vue ({
+    el: '#vue-app',
+    data : {
+        header : 'Data Binding',
+        website : 'https://MicroHawk.me',
+        value : 'Add Something',
+
+        websiteTag : '<a href="http://MicroHawk.me">This is my website</a>'
+    }
+
+
+});
