a89af52dc6579631bcb4eff15915d1ae421d48bb
micro-hawk <dasvikas0071@gmail.com>
2021-08-02 14:37:37 +0530

    Event buttons

--- events/app.js
+++ events/app.js
@@ -2,14 +2,14 @@ new Vue ({
     el:'#vue-app',
     data : {
         heading : 'Events In VUE',
-        quantity : '1'
+        quantity : 1
     },
     methods : {
-        add : function() {
-            this.quantity++;
+        add : function(inc) {
+            this.quantity += inc;
         },
-        sub : function(){
-            this.quantity--;
+        sub : function(dec){
+            this.quantity -= dec;
         }
     }
 });
