613793123f3b2188cabb7bb3001c7cc2db2c0bd2
micro-hawk <dasvikas0071@gmail.com>
2021-08-03 15:03:59 +0530

    Keyboard Listener

--- event-listner/app.js
+++ event-listner/app.js
@@ -10,6 +10,21 @@ new Vue ({
         updateXY : function(event) {
             this.x = event.offsetX ;
             this.y = event.offsetY;
+        },
+
+        clicked: function() {
+            alert("Hey you just clicked this shit :(");
+        },
+        
+        btnFunct: function(){
+            alert("U can click it for once only");
+        },
+
+        logUsername: function() {
+            console.log("You entered your user name");
+        },
+        logPassword : function() {
+            console.log("Password is entered.");
         }
     }
 
